6037cf5f019eb27d9b65dae102851f75abede768
Ren Hiyama <renhiyama@rovelstars.com>
2023-03-02 06:43:09 +0000

    daily updates

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="og:image" content="/logo.svg" />
+        <meta name="twitter:card" content="summary_small_image" />
+        <meta name="twitter:image" content="/logo.svg" />
+        <meta name="twitter:site" content="@rovelstars" />
+        <meta name="favicon" href="/logo.svg" />
+      </Head>
       <body
         className="!block min-h-screen bg-gray-100 dark:bg-black dark:text-white"
         style={{
